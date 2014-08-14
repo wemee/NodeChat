956fb0de5923313f864aac190333922124d90580
wemee <wemee7012@gmail.com>
2014-08-14 13:38:01 +0800

    scoket 訊息佇列優化

--- socket.js
+++ socket.js
@@ -4,23 +4,33 @@ function create(server){
 	var io = require('socket.io').listen(server);
 	var msg_queue = [];
 
+	var slimMsg = function(){
+  	if(msg_queue.length > 20){
+  		msg_queue = msg_queue.slice(msg_queue.length-15, msg_queue.length);
+  	}
+  };
+
+  var emitAllMsgTo = function(socket){
+  	for(var idx in msg_queue){
+	  	socket.emit('echo_back', msg_queue[idx]);
+	  }
+  }
+
+  setInterval(function(){
+	 	slimMsg();
+	}, 30*60*1000);
+
 	io.sockets.on('connection', function(socket) {
 	  console.log("A user connected, socket id: " + socket.id);
 
-	  for(var idx in msg_queue){
-	  	socket.emit('echo_back', msg_queue[idx]);
-	  	if(idx > 30)
-	  		break;
-	  }
+	  slimMsg();
+	  emitAllMsgTo(socket);
 
 	  socket.on('disconnect', function () {
 	    console.log("A user disconnect, socket id: " + socket.id);
 	  });
 
 	  socket.on('echo_post', function(name, data){
-	  	if (data.length <= 1)
-	  		return;
-
 	  	var date = new Date();
 	  	console.log("A user echo post, socket id: [" + socket.id + "], name: [" + name + "], data" + ": [" + data + "]");
 	  	fs.appendFile('message.txt', data+"<br>", function (err) {});
@@ -29,14 +39,6 @@ function create(server){
 	  	io.sockets.emit("echo_back", echo_msg);
 	  	msg_queue.push(echo_msg);
 	  });
-
-
-	  // setInterval(function(){
-	  // 	if(msg_queue.length > 60){
-	  // 		// msg_queue.shift();
-	  // 		msg_queue = msg_queue.slice(msg_queue.length-45, msg_queue.length)
-	  // 	}
-	  // }, 30*60*1000);
 	});
 }
 
